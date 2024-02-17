0929a58ed2bc7a4d5676a5e63fa4338f040de17b
Govind ghosh <govindghosh0@gmail.com>
2024-02-18 00:05:16 +0530

    [BUILD] npm i dotenv

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hey");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/govind", (req, res) => {
+  res.send("hey GOVIND GHOSH");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
