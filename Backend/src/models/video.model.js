291f79b6604da521076088c84b8f2f2a670f3454
Govind ghosh <govindghosh0@gmail.com>
2024-02-25 14:25:55 +0530

    [Build]models user and Video

---
+++ Backend/src/models/video.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudnery
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudnery
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
