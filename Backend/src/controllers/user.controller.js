0bdb1282942a3a309795a1f9d0708b5166f3acea
Govind ghosh <govindghosh0@gmail.com>
2024-03-11 11:24:46 +0530

    [BUILD]user.controller.js/ LoginUser

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,6 +4,32 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+// Function to generate access and refresh tokens for a given user ID
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    // Find the user by their ID
+    const user = await User.findById(userId);
+
+    // Generate a refresh token for the user
+    const refreshToken = user.generateRefreshToken();
+
+    // Generate an access and refresh token for the user
+    const accessToken = user.generateAccessToken();
+    user.refreshToken = refreshToken;
+
+    // Save the user object to the database, disabling validation before saving
+    await user.save({ validateBeforeSave: false });
+
+    // Return the refresh token and access token
+    return { refreshToken, accessToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating Access and Refresh Token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // res.status(200).json({
   //   message: "ok",
@@ -65,4 +91,34 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // get user Credentials from body
+  const { email, username, password } = req.body;
+  if (!email || !username) {
+    throw new ApiError(400, "username and email is required");
+  }
+  // find user Credentials from DB
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "user does not exist");
+  }
+  // check password is correct
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user Credentials");
+  }
+  // Call the generateAccessAndRefreshTokens function to generate tokens for the user with the specified ID,
+  // and destructure the returned object to extract the refreshToken and accessToken
+  const { refreshToken, accessToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+  // Find the user by their ID, excluding the password and refresh token fields from the retrieved document,
+  // and assign the result to the loggedInUser variable
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+});
+export { registerUser, loginUser };
